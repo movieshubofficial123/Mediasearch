67a875073751a00137ae11d2495dc7c13d6493be
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-08-18 11:20:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -329,8 +329,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('More Bots', url='https://t.me/AbinCr'),
+                        InlineKeyboardButton('Update Channel', url='https://t.me/MH_Newrelease1')
                     ]
                     ]
                 
